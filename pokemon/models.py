cd803509bda53b49ef0cf4c2a85dad0d657950b4
JesusGG208 <jgomgar208@g.educaand.es>
2025-04-28 13:42:13 +0200

    añadiendo modelos app pokemon

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    primary_type = models.CharField(max_length=200)
+    secondary_type = models.CharField(max_length=200)
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    special_attack = models.IntegerField()
+    special_deffense = models.IntegerField()
+    speed = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
