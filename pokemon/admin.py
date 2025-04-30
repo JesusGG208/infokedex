49485c1009f4abcb3b46c4c710006e60f627faca
JesusGG208 <jgomgar208@g.educaand.es>
2025-04-30 12:42:32 +0200

    Admin app pokemon

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from pokemon.models import Pokemon, EvolutionChain
 
 # Register your models here.
+admin.site.register(Pokemon)
+admin.site.register(EvolutionChain)
+
