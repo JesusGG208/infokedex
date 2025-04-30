621e621545632c50680e3d78341329091882a46a
JesusGG208 <jgomgar208@g.educaand.es>
2025-04-30 12:41:13 +0200

    Modelos app pokemon actualizado

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,16 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class EvolutionChain(models.Model):
+    """
+    Representa una cadena evolutiva completa.
+    """
+    chain_id = models.IntegerField(unique=True)  # ID de la PokeAPI
+
+    def __str__(self):
+        return f"Evolution Chain {self.chain_id}"
+
+
 class Pokemon(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    primary_type = models.CharField(max_length=200)
-    secondary_type = models.CharField(max_length=200)
+    """
+    Toda la información de un pokemon.
+    """
+    name = models.CharField(max_length=100, unique=True)
+    pokedex_id = models.IntegerField(unique=True)
+    primary_type = models.CharField(max_length=100)
+    secondary_type = models.CharField(max_length=100, null=True, blank=True)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    sprite_url = models.URLField()
+
+    # Estadísticas base
     hp = models.IntegerField()
     attack = models.IntegerField()
     defense = models.IntegerField()
     special_attack = models.IntegerField()
-    special_deffense = models.IntegerField()
+    special_defense = models.IntegerField()
     speed = models.IntegerField()
 
+    # Evolución
+    evolution_chain = models.ForeignKey(EvolutionChain, on_delete=models.SET_NULL, null=True, related_name='pokemons')
+    evolution_stage = models.IntegerField()
+
     def __str__(self):
-        return f"{self.name}"
+        return self.name
