3bcfc43d2ecedab74fb5120ef547615abf26ae89
JesusGG208 <jgomgar208@g.educaand.es>
2025-05-05 12:22:27 +0200

    Añadido modelos a la app compare

--- compare/models.py
+++ compare/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from pokemon.models import Pokemon
 
-# Create your models here.
+class Comparison(models.Model):
+    pokemon_1 = models.ForeignKey(Pokemon, related_name="pokemon_1", on_delete=models.CASCADE)
+    pokemon_2 = models.ForeignKey(Pokemon, related_name="pokemon_2", on_delete=models.CASCADE)
+    winner = models.ForeignKey(Pokemon, related_name="pokemon_winner", on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_draw(self):
+        return self.winner is None
+
+    def __str__(self):
+        if self.is_draw():
+            return f"{self.pokemon_1.name} vs {self.pokemon_2.name} → Empate"
+        return f"{self.pokemon_1.name} vs {self.pokemon_2.name} → Ganador: {self.winner.name}"
