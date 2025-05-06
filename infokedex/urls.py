7f720177e48a3d7169c90a0b6d23aa9dbebe25e4
EsDsVandal <jgomgar208@g.educaand.es>
2025-05-07 00:35:46 +0200

    Importada debug-toolbar

--- infokedex/urls.py
+++ infokedex/urls.py
@@ -16,10 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pokemon.urls')),
     path('compare/', include('compare.urls')),
     path('pokewordle/', include('pokewordle.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
