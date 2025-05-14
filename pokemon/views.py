74e021d54482182656c803d5522c3a0863eb75bf
jesus <jgomgar208@g.educaand.es>
2025-05-14 14:54:21 +0200

    Buscador actualizado

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,9 +1,8 @@
 import random
-
 from django.http import JsonResponse
 from django.views import View
 from django.views.generic import DetailView, ListView, TemplateView
-
+from django.db.models import Q
 from pokemon.models import Pokemon
 
 
@@ -14,15 +13,15 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         pokemon = list(Pokemon.objects.all())
         random.shuffle(pokemon)
-        context['pokemon'] = pokemon[:8]
+        context['pokemon'] = pokemon[:10]
         return context
 
 class SearchView(View):
     def get(self, request):
         q = request.GET.get('q', '')
-        pokemon = Pokemon.objects.filter(name__icontains=q)[:10]
+        pokemon = Pokemon.objects.filter(Q(name__icontains=q) | Q(pokedex_id__icontains=q))[:10]
         return JsonResponse(
-            [{'name': p.name, 'id': p.pokedex_id} for p in pokemon],
+            [{'name': p.name, 'pokedex_id': p.pokedex_id, 'sprite': p.sprite_default} for p in pokemon],
             safe=False
         )
 
