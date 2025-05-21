a58288579989c070e709c32ea25105e5b9461eff
EsDsVandal <jgomgar208@g.educaand.es>
2025-05-21 21:03:37 +0200

    Eliminado PokemonFilterView y PokemonListView modificado

--- pokemon/views.py
+++ pokemon/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from django.views.generic import DetailView, ListView, TemplateView
 from django.db.models import Q
 from pokemon.models import Pokemon, Type
-
+from django.shortcuts import render
 
 class HomePageView(TemplateView):
     template_name = 'all_templates/home.html'
@@ -28,72 +28,46 @@ class SearchView(View):
 class PokemonListView(ListView):
     model = Pokemon
     template_name = 'all_templates/pokemon_list.html'
-    context_object_name = 'pokemon'
+    context_object_name = 'pokemons'
 
-class PokemonFilterView(View):
-    def get(self, request):
-        q = request.GET.get('q')
-        type_ = request.GET.get('type')
-        generation = request.GET.get('generation')
-        evo_stage = request.GET.get('evolution_stage')
+    def get_queryset(self):
+        queryset = Pokemon.objects.all()
 
-        pokemon = Pokemon.objects.all()
-
-        # Buscador
-        if q:
-            pokemon = pokemon.filter(
-                Q(name__icontains=q) | Q(pokedex_id__icontains=q)
+        search_query = self.request.GET.get('q', '').strip()
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(pokedex_id__icontains=search_query)
             )
 
-        # Tipo
-        if type_:
-            pokemon = pokemon.filter(types__type__name=type_)
+        type_filter = self.request.GET.get('type')
+        if type_filter:
+            queryset = queryset.filter(types__type__name=type_filter)
 
-        # Generación
-        if generation:
-            try:
-                generation = int(generation)
-                pokemon = pokemon.filter(generation=generation)
-            except ValueError:
-                pass
+        gen_filter = self.request.GET.get('generation')
+        if gen_filter:
+            queryset = queryset.filter(generation=gen_filter)
 
-        # Etapa evolutiva
+        evo_stage = self.request.GET.get('evolution_stage')
         if evo_stage:
-            try:
-                evo_stage = int(evo_stage)
-                if evo_stage == 1:
-                    pokemon = pokemon.filter(
-                        evolves_from__isnull=True,
-                        evolves_to__isnull=False
-                    )
-                elif evo_stage == 2:
-                    pokemon = pokemon.filter(
-                        evolves_from__isnull=False,
-                        evolves_to__isnull=False
-                    )
-                elif evo_stage == 3:
-                    pokemon = pokemon.filter(
-                        evolves_from__isnull=False,
-                        evolves_to__isnull=True
-                    )
-                elif evo_stage == 4:
-                    pokemon = pokemon.filter(
-                        evolves_from__isnull=True,
-                        evolves_to__isnull=True
-                    )
+            if evo_stage == '1':
+                queryset = queryset.filter(evolves_from__isnull=True)
+            elif evo_stage == '2':
+                queryset = queryset.filter(evolves_from__isnull=False, evolves_to__isnull=False)
+            elif evo_stage == '3':
+                queryset = queryset.filter(evolves_to__isnull=True)
+            elif evo_stage == '4':
+                # Sin evolución
+                queryset = queryset.filter(evolves_from__isnull=True, evolves_to__isnull=True)
 
-            except ValueError:
-                pass
+        return queryset.distinct()
 
-        pokemon = pokemon.distinct()[:1025]
-
-        data = [{
-            'name': p.name,
-            'pokedex_id': p.pokedex_id,
-            'sprite': p.sprite_default
-        } for p in pokemon]
-
-        return JsonResponse(data, safe=False)
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+        context['types'] = Type.objects.all()
+        context['generations'] = range(1, 10)
+        return render(request, self.template_name, context)
 
 class PokemonDetailView(DetailView):
     model = Pokemon
