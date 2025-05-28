afc37abf3c76ed9d286d53701e6e9cc33ecc8528
EsDsVandal <jgomgar208@g.educaand.es>
2025-05-28 02:50:14 +0200

    Models y views modificados

--- compare/models.py
+++ compare/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from pokemon.models import Pokemon
 
-class Comparison(models.Model):
+class Compare(models.Model):
     pokemon_1 = models.ForeignKey(Pokemon, related_name="pokemon_1", on_delete=models.CASCADE)
     pokemon_2 = models.ForeignKey(Pokemon, related_name="pokemon_2", on_delete=models.CASCADE)
     winner = models.ForeignKey(Pokemon, related_name="pokemon_winner", on_delete=models.SET_NULL, null=True, blank=True)
@@ -11,38 +11,43 @@ class Comparison(models.Model):
         return self.winner is None
 
     def calculate_winner(self):
-        stats = [
-            'hp',
-            'attack',
-            'defense',
-            'special_attack',
-            'special_defense',
-            'speed',
-        ]
-
-        p1_score = 0
-        p2_score = 0
-
-        for stat in stats:
-            stat_1 = getattr(self.pokemon_1, stat, 0)
-            stat_2 = getattr(self.pokemon_2, stat, 0)
-            if stat_1 > stat_2:
-                p1_score += 1
-            elif stat_2 > stat_1:
-                p2_score += 1
+        stats = ['hp', 'attack', 'defense', 'special_attack', 'special_defense', 'speed']
 
         # Comparar total sin modificar modelo Pokemon
         total_1 = sum(getattr(self.pokemon_1, stat, 0) for stat in stats)
         total_2 = sum(getattr(self.pokemon_2, stat, 0) for stat in stats)
 
         if total_1 > total_2:
-            p1_score += 1
-        elif total_2 > total_1:
-            p2_score += 1
-
-        if p1_score > p2_score:
             self.winner = self.pokemon_1
-        elif p2_score > p1_score:
+        elif total_2 > total_1:
             self.winner = self.pokemon_2
         else:
             self.winner = None  # Empate
+
+    def get_stat_comparisons(self):
+        stats = ['hp', 'attack', 'defense', 'special_attack', 'special_defense', 'speed']
+        result = {}
+
+        for stat in stats:
+            val1 = getattr(self.pokemon_1, stat, 0)
+            val2 = getattr(self.pokemon_2, stat, 0)
+
+            if val1 > val2:
+                result[stat] = ['stat-better', 'stat-worse']
+            elif val2 > val1:
+                result[stat] = ['stat-worse', 'stat-better']
+            else:
+                result[stat] = ['stat-equal', 'stat-equal']
+
+        # Calcula el total también
+        total1 = sum(getattr(self.pokemon_1, stat, 0) for stat in stats)
+        total2 = sum(getattr(self.pokemon_2, stat, 0) for stat in stats)
+
+        if total1 > total2:
+            result['total'] = ['stat-better', 'stat-worse']
+        elif total2 > total1:
+            result['total'] = ['stat-worse', 'stat-better']
+        else:
+            result['total'] = ['stat-equal', 'stat-equal']
+
+        return result
--- compare/views.py
+++ compare/views.py
@@ -1,39 +1,21 @@
 from django.views.generic import FormView
-from django.shortcuts import get_object_or_404
-from .models import Comparison
-from pokemon.models import Pokemon
-from .forms import ComparisonForm
+from .forms import CompareForm
+from .models import Compare
+
 
 class PokemonCompareView(FormView):
     template_name = "compare.html"
-    form_class = ComparisonForm
+    form_class = CompareForm
 
     def form_valid(self, form):
-        p1 = get_object_or_404(Pokemon, id=form.cleaned_data['pokemon_1'].id)
-        p2 = get_object_or_404(Pokemon, id=form.cleaned_data['pokemon_2'].id)
+        pokemon_1 = form.cleaned_data['pokemon_1']
+        pokemon_2 = form.cleaned_data['pokemon_2']
 
-        stats = ["hp", "attack", "defense", "special_attack", "special_defense", "speed"]
+        comparison = Compare(pokemon_1=pokemon_1, pokemon_2=pokemon_2)
+        comparison.calculate_winner()
+        comparison.save()
 
-        p1_score = sum(getattr(p1, stat) for stat in stats)
-        p2_score = sum(getattr(p2, stat) for stat in stats)
-
-        if p1_score > p2_score:
-            winner = p1
-        elif p2_score > p1_score:
-            winner = p2
-        else:
-            winner = None
-
-        comparison = Comparison.objects.create(
-            pokemon_1=p1, pokemon_2=p2, winner=winner
-        )
-
-        context = self.get_context_data(
-            form=form,
-            result={
-                "comparison": comparison,
-                "p1_score": p1_score,
-                "p2_score": p2_score,
-            }
-        )
+        context = self.get_context_data(form=form)
+        context['comparison'] = comparison
+        context['stat_colors'] = comparison.get_stat_comparisons()
         return self.render_to_response(context)
