d801386b36ca207bb341fad7a1e3d66ba21f53fa
EsDsVandal <jgomgar208@g.educaand.es>
2025-05-28 02:49:10 +0200

    forms.py de app Compare modificado

--- compare/forms.py
+++ compare/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from pokemon.models import Pokemon
 
-class ComparisonForm(forms.Form):
+class CompareForm(forms.Form):
     pokemon_1 = forms.ModelChoiceField(queryset=Pokemon.objects.all(), label="Pokémon 1")
     pokemon_2 = forms.ModelChoiceField(queryset=Pokemon.objects.all(), label="Pokémon 2")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        pokemon_1 = cleaned_data.get("pokemon_1")
+        pokemon_2 = cleaned_data.get("pokemon_2")
+
+        if pokemon_1 == pokemon_2:
+            raise forms.ValidationError("Debes seleccionar dos Pokémon diferentes.")
+
+        return cleaned_data
