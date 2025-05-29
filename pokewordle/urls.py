9c6531121330f9d28aeb68a1727c7b1ad27d718a
EsDsVandal <jgomgar208@g.educaand.es>
2025-05-30 01:18:28 +0200

    A empezar el PokéWordle de cero

--- pokewordle/urls.py
+++ pokewordle/urls.py
@@ -4,6 +4,5 @@ from pokewordle import views
 app_name = 'pokewordle'
 
 urlpatterns = [
-    path('', views.StartPokewordleGame.as_view(), name='start'),
-    path('play/<int:pk>/', views.PlayPokewordleGame.as_view(), name='play'),
+
 ]
